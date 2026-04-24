4bec597db237c7022496d0287c9376dd5d531dff
saim1sh <saim.sh0100@gmail.com>
2026-04-24 13:21:23 +0600

    .

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,7 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Replace with your actual Firebase config or add them to a .env file (e.g. VITE_FIREBASE_API_KEY)
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
